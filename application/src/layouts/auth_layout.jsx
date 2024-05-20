e20252d8efdbf50d5137f2b5d02a0ea03e3d04dd
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2024-05-20 20:38:43 +0700

    [modify] - modify auth layout

--- application/src/layouts/auth_layout.jsx
+++ application/src/layouts/auth_layout.jsx
@@ -1,8 +1,10 @@
+import bg_auth from "../assets/bg-auth.jpg"
+
 export default function AuthLayout({ children }) {
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
             <div className="w-full max-w-screen-sm shadow-md drop-shadow-md border">
-                <div className="w-full h-[250px] bg-blue-400"></div>
+                <img src={bg_auth} className="w-full h-[250px] object-cover" alt="" srcset="" />
                 <div className="p-4">
                     {children}
                 </div>
